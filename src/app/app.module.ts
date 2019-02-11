f5a094020fcf371ce9e6ea35efd9f239981d971e
Anvesha123 <45240609+Anvesha123@users.noreply.github.com>
2019-02-11 15:39:30 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { RegistrationService } from './registrationService';
+import { RegistrationComponent } from './registration.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+ 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
+    AppComponent,RegistrationComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,HttpClientModule
   ],
-  providers: [],
+  providers: [RegistrationService,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
